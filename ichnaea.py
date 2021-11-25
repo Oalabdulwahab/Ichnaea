fed47ee127aefafcb95a5ca2df5634132cef7f24
Omar Alabdulwahab <36615618+Oalabdulwahab@users.noreply.github.com>
2021-11-25 17:36:22 +0300

    Update ichnaea.py

--- ichnaea.py
+++ ichnaea.py
@@ -17,7 +17,7 @@ print("""
   _| |__  \ \___    ) )( (    / /  \ \/ /    /  /\  \   \ \___   /  /\  \  
  /_____(   \____)  /_/  \_\  (_/    \__/    /__(  )__\   \____\ /__(  )__\ 
                                                                            
-[*] Version 0.1 Beta 
+[*] Version 1.1 
 [*] Compare IIS ApplicationHost files
 """) 
 
@@ -34,33 +34,37 @@ class disable_file_system_redirection:
 
 
 class Ichnaea:
-    def __init__(self, file_path, history_path, extenstion, output):
-        self.extenstion = extenstion
+    def __init__(self, file_path, history_path, output, mode):
         self.config_files = self.list_dir(file_path)
         self.history_files = self.list_dir(history_path) + self.config_files
-        self.tags = {}
-        self.section = ['section', 'application']
-        self.IIS_Sites = self.get_IIS_Sites_details(self.config_files[0])
-        self.IIS_Modules = self.get_IIS_modules_details(self.config_files[0])
-        self.mydata = []
-        rfile = range(len(self.history_files) - 1)
-        count = 0
-        countnew = 1
-        for count in rfile:
-            self.diff_files(self.history_files[count], self.history_files[countnew])
-            count += 1
-            countnew = count + 1
-        compareCols = ['History Filename', 'Modification Time', 'Added Lines', 'Removed Lines']
-        sitesCols = ['Site name', 'Site id', 'path', 'Virtual Path', 'Physical Path']
-        modulesCols = ['name', 'image']
-        if output == "json":
-            self.to_json(self.mydata, "CompareResult.json")
-            self.to_json(self.IIS_Sites, "IIS_Sites.json")
-            self.to_json(self.IIS_Modules, "IIS_Modules.json")
-        else:
-            self.to_csv(self.mydata, "CompareResult.csv", compareCols)
-            self.to_csv(self.IIS_Sites, "IIS_Sites.csv", sitesCols)
-            self.to_csv(self.IIS_Modules, "IIS_Modules.csv", modulesCols)
+        if mode == "All" or mode == "Sites":
+            IIS_Sites = self.get_IIS_Sites_details(self.config_files[0])
+            sitesCols = ['Site name', 'Site id', 'path', 'Virtual Path', 'Physical Path']
+            if output == "json":
+                self.to_json(IIS_Sites, "IIS_Sites.json")
+            else:
+                self.to_csv(IIS_Sites, "IIS_Sites.csv", sitesCols)
+        if mode == "All" or mode == "Modules":
+            IIS_Modules = self.get_IIS_modules_details(self.config_files[0])
+            modulesCols = ['name', 'image']
+            if output == "json":
+                self.to_json(IIS_Modules, "IIS_Modules.json")
+            else:
+                self.to_csv(IIS_Modules, "IIS_Modules.csv", modulesCols)
+        if mode == "All" or mode == "Compare":
+            self.compareResult = []
+            rfile = range(len(self.history_files) - 1)
+            count = 0
+            countnew = 1
+            for count in rfile:
+                self.diff_files(self.history_files[count], self.history_files[countnew])
+                count += 1
+                countnew = count + 1
+                compareCols = ['History Filename', 'Modification Time', 'Added Lines', 'Removed Lines']
+            if output == "json":
+                self.to_json(self.compareResult, "CompareResult.json")
+            else:
+                self.to_csv(self.compareResult, "CompareResult.csv", compareCols)
 
     # list csv file in a given path
     def list_dir(self, path):
@@ -68,10 +72,10 @@ class Ichnaea:
         with disable_file_system_redirection():
             for root, dirs, files in os.walk(path):
                 configFiles += [os.path.join(root, f)
-                                for f in files if f.endswith(self.extenstion)]
+                                for f in files if f.endswith("applicationHost.config")]
             configFiles.sort()
             if not configFiles:
-                print("No config files were found")
+                print("No config files were found in " + path)
         return configFiles
 
     # Parse IIS glbal modules from Config File
@@ -117,12 +121,12 @@ class Ichnaea:
                 dic_data = {'History Filename': new, 'Modification Time': time.ctime(os.path.getmtime(new))} 
                 dic_data.setdefault('Removed Lines', [])
                 dic_data['Removed Lines'].append(comp.strip("- "))
-                self.mydata.append(dic_data)
+                self.compareResult.append(dic_data)
             elif comp.startswith('+'):
                 dic_data = {'History Filename': new, 'Modification Time': time.ctime(os.path.getmtime(new))} 
                 dic_data.setdefault('Added Lines', [])
                 dic_data['Added Lines'].append(comp.strip().strip("+ "))
-                self.mydata.append(dic_data)
+                self.compareResult.append(dic_data)
 
     # write compare result to csv file
     def to_csv(self, file, filename, cols):  
@@ -144,29 +148,23 @@ class Ichnaea:
         except IOError:
             print("I/O error")
 
-
+# Main function
 def main():
     parser = argparse.ArgumentParser(
-        description="Read IIS config files history ", usage="will c  ",formatter_class=argparse.RawTextHelpFormatter)
+        description="IIS Configuration files analyzer", usage="ichnaea [-h] [-f CONFIGPATH] [-p HISTORYPATH] [-o OUTPUTFORMAT] [-m MODE]",formatter_class=argparse.RawTextHelpFormatter)
 
-    parser.add_argument('-f', '--file', help="Path of ApplicationHost.config file (default: %%windir%%\\system32\\inetsrv\\config)",default="%%windir%%\\system32\\inetsrv\\config", required=False)
-    parser.add_argument('-p', '--files_path', help="Path of ApplicationHost.config history files (default: C:\\inetpub\\history)", required=False)
+    parser.add_argument('-f', '--file', help="Path of ApplicationHost.config file (default: C:\\Windows\\system32\\inetsrv\\config)",default="C:\\Windows\\system32\\inetsrv\\config", required=False)
+    parser.add_argument('-p', '--files_path', help="Path of ApplicationHost.config history files (default: C:\\inetpub\\history)", default="C:\\inetpub\\history", required=False)
     parser.add_argument('-o', '--output_type', help="File Output type",choices=['json', 'csv'], default="csv", required=False )
-    parser.add_argument('-m','--mode',choices=['Compare', 'Sites', 'Modules'],
-        help="Compare: used to compare history files and get the result\n" 
+    parser.add_argument('-m','--mode',choices=['All','Compare', 'Sites', 'Modules'], default="All",
+        help="All: run all modes (default)\n"
+        "Compare: used to compare history files and get the result\n" 
         "Sites: used to parse the IIS sites\n"
         "Modules: used to parse the IIS global (Native) modules\n" 
         , required=False)
     args = parser.parse_args()
-    ConfFile = 'C:\\Windows\\system32\\inetsrv\\config'
-    historyFilesPath = 'C:\\inetpub\\history'
-    if args.file:
-        ConfFile = args.file
-    if args.files_path:
-        historyFilesPath = args.files_path
 
-    Ichnaea(ConfFile, historyFilesPath, "applicationHost.config", args.output_type)
-
+    Ichnaea(args.file, args.files_path, args.output_type, args.mode)
 
 if __name__ == "__main__":
     main()
